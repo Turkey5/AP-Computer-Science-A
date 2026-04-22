760f6abd1098e7556275ac5110ace1ff806e80e3
Turkey5 <turkteq@gmail.com>
2026-04-22 08:19:11 -0500

    Added U1_L1_Activity_Three
    This program outlines the steps to make a peanut butter and jam sandwich.

---
+++ Unit 1/Lesson 1/U1_L1_Activity_Three.java
@@ -0,0 +1,21 @@
+/* Lesson 1 Coding Activity Question 3 */
+
+class U1_L1_Activity_Three {
+  public static void main(String[] args) {
+    // im gonna do jam because it tastes better
+    System.out.println("1. Obtain peanutbutter");
+    System.out.println("2. Obtain jam");
+    System.out.println("3. Obtain 2 slices of bread");
+    System.out.println("3.1. Optionally toast the bread");
+    System.out.println("4. Obtain a knife");
+    System.out.println("4.1 Optionally obtain another knife or spoon for jam to avoid cross-contamination");
+    System.out.println("5. Open the jar of peanutbutter");
+    System.out.println("6. Open the jar of jam");
+    System.out.println("7. Use your utensil of choice to spread a generous amount of peanut butter on the face of one of the bread pieces");
+    System.out.println("8. Use your utensil of choice to spread a smaller amount of jam on the face of the other piece of bread");
+    System.out.println("9. Put the two pieces of bread together, combining the peanutbutter and the jam");
+    System.out.println("10. Enjoy");
+
+    // While this many steps is probably unnecessary, it showcases println() functionality to create new lines
+  }
+}
