f5f85eba8fb2c43f5dbaa5a77aed04eb7e8237b6
Turkey5 <turkteq@gmail.com>
2026-04-22 08:16:02 -0500

    Added U1_L1_Activity_Two.javay

---
+++ Unit 1/Lesson 1/U1_L1_Activity_Two.java
@@ -0,0 +1,14 @@
+/* Lesson 1 Coding Activity Question 2 */
+
+class U1_L1_Activity_Two {
+  public static void main(String[] args) {
+    System.out.println(" Coding is fun! ");
+    System.out.println(" Coding is fun! ");
+    System.out.println(" Coding is fun! ");
+
+    // by using println() instead of print(), each statement will start a new line after being printed
+    // if we only used the print() method, the output would look like this:
+    // " Coding is fun!  Coding is fun!  Coding is fun! "
+    // note how there are no new lines being created
+  }
+}
