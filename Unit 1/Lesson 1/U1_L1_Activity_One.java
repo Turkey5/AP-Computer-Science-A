bbc4bdd923f7e76a1a79697ad6436af4629b120a
Turkey5 <turkteq@gmail.com>
2026-04-22 08:13:09 -0500

    Added Unit 1 Lesson 1 Coding Activity 1
    Added comments to explain the System.out.println() usage and string handling.

---
+++ Unit 1/Lesson 1/U1_L1_Activity_One.java
@@ -0,0 +1,11 @@
+/* Lesson 1 Coding Activity Question 1 */
+
+class U1_L1_Activity_One {
+  public static void main(String[] args) {
+    System.out.println("     Stephen Curry     ");
+    // System.out.println() will print the contents to the terminal
+    // in this case, it will print the string: "     Stephen Curry     "
+    // note that the quotation marks define the text as a String, and will not be printed
+    // also note that without the quotation marks, the text will not be identified as a String, resulting in a Runtime Error
+  }
+}
